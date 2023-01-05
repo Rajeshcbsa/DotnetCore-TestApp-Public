9e01eff3f90e8747ebaa5f2812188bd3123ef73a
Rajeshcbsa <rajeshcbsa@yahoo.co.in>
2023-01-05 21:46:00 +0530

    Add project files.

---
+++ DotnetCore-TestApp-Public/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotnetCore_TestApp_Public.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
